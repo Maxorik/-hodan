a61c8fe6228e54ea99f234bdec40cd4132dfba33
Your Name <gms441@mail.ru>
2024-04-05 09:44:43 +0700

    optimize image

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { observer } from "mobx-react-lite";
-import appStore from 'store';
+import appStore, { isMobile } from 'store';
 
 export const MainPage = observer(() => {
     return (
         <div className='content-container welcome-page'>
             <p className='center'>&bull; Добро пожаловать! &bull;</p>
-            <img className='welcome-preview' src='../assets/welcome.webp' />
+            <img className='welcome-preview' src={ isMobile ? '../assets/welcome_phone.webp' : '../assets/welcome.webp' } />
             <div className='main-nav-container'>
                 <p onClick={ () => appStore.setActivePage('resources') }>
                     <span className='divider-mobile'>&bull;</span>
